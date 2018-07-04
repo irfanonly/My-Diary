9f30be86910b4fa0e45767d5ed5cd90d26848bd2
irfan mohammed <mamirfan92@gmail.com>
2018-07-04 23:02:38 +0530

    test cases written

--- UnitTestProject/HomeControllerTest.cs
+++ UnitTestProject/HomeControllerTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyDiary.Controllers;
+using MyDiary.Models;
 
 namespace UnitTestProject
 {
@@ -10,11 +11,41 @@ namespace UnitTestProject
         [TestMethod]
         public void Index_ShouldReturn()
         {
+            //Arrange
             var controller = new HomeController();
+            //Act
             var result = controller.Index() as ViewResult;
+            //Assert
             Assert.IsNotNull(result);
 
             Assert.AreEqual(result.ViewName,"Index");
         }
+        [TestMethod]
+        public void About_ShouldReturn()
+        {
+            //Arrange
+            var controller = new HomeController();
+            //Act
+            var result = controller.About() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(result.ViewData["Message"], "Your application description page.");
+        }
+
+        [TestMethod]
+        public void Contact_ShouldReturn()
+        {
+            //Arrange
+            var controller = new HomeController();
+            //Act
+            var result = controller.Contact() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(result.ViewData["Message"], "Your contact page.");
+        }
+
+        
     }
 }
