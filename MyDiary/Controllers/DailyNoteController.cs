1304d39b2730d58f07826e1632c5298a9fb1962a
irfan mohammed <mamirfan92@gmail.com>
2018-07-11 23:08:32 +0530

    unit test updated

--- MyDiary/Controllers/DailyNoteController.cs
+++ MyDiary/Controllers/DailyNoteController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DAL;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyDiary.Controllers
 {
     public class DailyNoteController : Controller
     {
+        private IDailyNotesDAL _dal;
+
+        public DailyNoteController() : this(new DailyNotesDAL())
+        {
+        }
+
+        public DailyNoteController(IDailyNotesDAL dal)
+        {
+            _dal = dal;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View("Index",_dal.GetAllDailyNotes());
         }
     }
 }
